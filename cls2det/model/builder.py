381c731ecc2504bf49a3842afda65c75170ba86b
YuxinZou <zouyux@outlook.com>
2020-06-11 15:39:06 +0800

    code updated

--- cls2det/model/builder.py
+++ cls2det/model/builder.py
@@ -4,17 +4,15 @@ from .model import resnet18
 
 
 def model_builder(cfg):
-    if cfg.gpu and torch.cuda.is_available():
+    if cfg.gpu is not None and torch.cuda.is_available():
         print('=> use GPU: {}'.format(cfg.gpu))
         device = torch.device(f'cuda:{cfg.gpu}')
     else:
         print('=> use CPU')
         device = torch.device('cpu')
 
-    if cfg.model.arch != 'resnet18':
-        raise Exception('currently this tool only support model "resnent18".')
-    print('=> building pre-trained model {}'.format(cfg.model.arch))
-    model = resnet18(pretrained=cfg.model.pre_trained)
+    print('=> building pre-trained model resnet18')
+    model = resnet18(pretrained=True)
 
     model = model.to(device)
     model.eval()
